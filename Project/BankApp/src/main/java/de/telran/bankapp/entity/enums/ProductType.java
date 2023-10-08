c1c548b5b3b5d7f3020335fd1ac91d3c86940676
DmytroFranchuk <124680129+DmytroFranchuk@users.noreply.github.com>
2023-10-08 15:54:22 +0200

    Delete Project/BankApp directory

--- Project/BankApp/src/main/java/de/telran/bankapp/entity/enums/ProductType.java
+++
@@ -1,6 +0,0 @@
-package de.telran.bankapp.entity.enums;
-
-public enum ProductType {
-    CREDIT_ACCOUNT, DEBIT_ACCOUNT, SAVING_ACCOUNT
-
-}
--- Project/BankApp/src/main/java/de/telran/bankapp/service/AccountService.java
+++
@@ -1,9 +0,0 @@
-package de.telran.bankapp.service;
-
-import de.telran.bankapp.entity.Account;
-
-import java.util.List;
-
-public interface AccountService {
-    List<Account> getAll();
-}
