ce642852aad4af13f2305a64e1ab0da07b902df6
Dmytro <franchuk.df@gmail.com>
2024-01-05 01:00:54 +0100

    java_dz_2023_11_07

---
+++ HomeWork/java_dz_2023_11_07/src/main/java/org/example/ExampleLombok.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import lombok.*;
+
+
+@Data
+@AllArgsConstructor
+public class ExampleLombok {
+    private Integer summa;
+    private String check;
+    private boolean validation;
+}
---
+++ HomeWork/java_dz_2023_11_07/src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        ExampleLombok eLombok = new ExampleLombok(12, "накопительный", true);
+        ExampleLombok eLombok1 = new ExampleLombok(21, "накопительный", true);
+        ExampleLombok eLombok2 = new ExampleLombok(21, "накопительный", true);
+        System.out.println(eLombok.getSumma());
+        System.out.println(eLombok.getCheck());
+        System.out.println(eLombok.isValidation());
+        eLombok.setSumma(100);
+        eLombok.setCheck("дебетовый счет с кэшбэком на продукты");
+        eLombok.setValidation(false);
+        System.out.println(eLombok);
+        System.out.println(eLombok.hashCode());
+        System.out.println(eLombok.equals(eLombok1));
+        System.out.println(eLombok1.canEqual(eLombok2));
+    }
+}
